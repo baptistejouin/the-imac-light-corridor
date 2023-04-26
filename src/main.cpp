7472ccd2c1cf996846c814e95a3f9cc4734bb423
Baptiste <baptiste.jouin44@gmail.com>
2023-04-26 12:53:58 +0000

    feat: add Gitpod

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
-#include <OpenGL/glu.h>
-#include <OpenGL/gl.h>
+#include <GL/glu.h>
+#include <GL/gl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
